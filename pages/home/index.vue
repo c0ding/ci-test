6baf14ca49569da0ac39a9f1ba3acb3b0c48cf98
Yvan <hzyvan@gmail.com>
2022-01-24 12:22:07 +0800

    init

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,8 +3,8 @@
 
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">拉勾教育</h1>
-        <p>A place to share your knowledge.</p>
+        <h1 class="logo-font">test cicd with github-actions</h1>
+        <p>github to share your code.</p>
       </div>
     </div>
 
